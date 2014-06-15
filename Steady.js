1c0bdcef18fa7fb9f38a0e97765d9def9a3f6fe7
Khalid Lafi <lafi.khalid@gmail.com>
2014-06-15 13:12:19 +0300

    release v0.3.0

--- Steady.js
+++ Steady.js
@@ -11,6 +11,7 @@ function Steady(opts) {
   this.success   = false;
   this.throttleVal = opts.throttle || 100;
   this.processing = false;
+  this.stopped = false;
 
 
   this._parse();
@@ -33,6 +34,10 @@ function Steady(opts) {
 
 Steady.prototype.addCondition = function(name, value) {
   this.conditions[name] = value;
+  this._parse();
+};
+Steady.prototype.removeCondition = function(name) {
+  delete this.conditions[name];
   this._parse();
 };
 Steady.prototype.addTracker  = function(name, fn) {
@@ -152,10 +157,13 @@ Steady.prototype._done = function() {
 };
 
 Steady.prototype._onScroll = function() {
-  var self = this;
-  
+  this._onScrollHandler = this._throttledHandler();
+  this.scrollElement.addEventListener('scroll', this._onScrollHandler, false);
+};
 
-  this.scrollElement.onscroll = this.throttle(function(e) {
+Steady.prototype._throttledHandler = function() {
+  var self = this;
+  return this.throttle(function(e) {
 
     if ( !self._wantedTrackers.length || self.processing ) return;
     
@@ -168,7 +176,19 @@ Steady.prototype._onScroll = function() {
     
     window.requestAnimationFrame(self._check.bind(self));
   }, this.throttleVal);
+};
 
+Steady.prototype.stop = function() {
+  if ( ! this.stopped  ) {
+    this.scrollElement.removeEventListener('scroll', this._onScrollHandler, false);
+    this.stopped = true;
+  }
+};
+
+Steady.prototype.resume = function() {
+  if ( this.stopped  ) 
+    this._onScroll();
+    this.stopped = false;
 };
 
 
---
+++ tests/test.js
@@ -0,0 +1,248 @@
+QUnit.config.scrolltop = false;
+
+
+QUnit.asyncTest('test stop method', function(assert) {
+  expect(1);
+  var runs = 0;  
+  console.log('runs', runs);
+  var s = new Steady({
+    conditions: {
+      'min-scrollY': 200,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      runs++;
+      console.log('runs', runs);
+      done();
+    }
+  });
+
+  window.scrollTo(0, 200);
+  
+  requestAnimationFrame(function() {
+
+    window.scrollTo(0, 205);
+
+    setTimeout(function() {
+      console.log('vsalllllls', runs);
+      s.stop();
+      
+      setTimeout(function() {
+        assert.equal(runs, 1, 'stop is okay' );
+        QUnit.start();
+      }, 200);
+
+    }, 20);
+  });
+});
+
+QUnit.asyncTest('test resume method', function(assert) {
+  expect(1);
+  var runs = 0;  
+  console.log('runs', runs);
+  var s = new Steady({
+    conditions: {
+      'min-scrollY': 200,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      runs++;
+      console.log('runs', runs);
+      done();
+    }
+  });
+
+  window.scrollTo(0, 200);
+  
+  requestAnimationFrame(function() {
+
+    s.stop();
+    window.scrollTo(0, 300);
+    setTimeout(function() {
+      
+      s.resume();
+      
+      setTimeout(function() {
+        
+        window.scrollTo(0, 310);
+        
+        setTimeout(function() {
+          assert.equal(runs, 2, 'resume method runs should equal 2' );
+          QUnit.start();
+        }, 200);
+      }, 200);
+
+    }, 20);
+  });
+  
+  
+});
+
+QUnit.asyncTest('test addCondition', function(assert) {
+  expect(1);
+  var s = new Steady({
+    conditions: {
+      'max-bottom': 200,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      done();
+    }
+  });
+
+  s.addCondition('max-scrollY', 200);
+
+  s.stop();
+
+  console.log(s);
+  assert.equal(s.conditions['max-scrollY'], 200, 'addCondition is okay' );
+  QUnit.start();
+});
+
+
+QUnit.asyncTest('test removeCondition', function(assert) {
+  expect(1);
+  var s = new Steady({
+    conditions: {
+      'max-bottom': 200,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      done();
+    }
+  });
+
+  s.removeCondition('max-bottom');
+
+  console.log(s);
+  s.stop();
+  assert.equal(s.conditions['max-bottom'], undefined, 'removeCondition is okay' );
+  QUnit.start();
+  
+});
+
+
+QUnit.asyncTest('test throttle', function(assert) {
+  expect(1);
+  var s = new Steady({
+    conditions: {
+      'max-bottom': 200,
+    },
+    throttle: 200,
+    handler: function(values, done) {
+      console.log(values);
+      done();
+    }
+  });
+
+  
+
+  console.log(s);
+  s.stop();
+  assert.equal(s.throttleVal, 200, 'throttleVal is okay' );
+  QUnit.start();
+});
+
+QUnit.asyncTest('test bottom tracker', function(assert) {
+  expect(1);
+  var s = new Steady({
+    conditions: {
+      'max-bottom': 200,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      assert.equal(values.bottom <= 200, true, 'Bottom is okay!!' );
+      QUnit.start();
+      s.stop();
+      done();
+    }
+  });
+
+  var to = ( document.body.scrollHeight - window.innerHeight ) - 180;
+  window.scrollTo(0, to);
+});
+
+QUnit.asyncTest('test scrollX tracker', function(assert) {
+  expect(1);
+  var s = new Steady({
+    conditions: {
+      'scrollX': 0,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      assert.equal(values.scrollX, 0, 'scrollX is okay' );
+      QUnit.start();
+      s.stop();
+      done();
+    }
+  });
+
+  window.scrollTo(0, 1);
+});
+
+QUnit.asyncTest('test scrollY tracker', function(assert) {
+  expect(1);
+  var s = new Steady({
+    conditions: {
+      'min-scrollY': 100,
+    },
+    handler: function(values, done) {
+      console.log(values);
+      assert.equal(values.scrollY >= 101, true, 'scrollX is okay' );
+      QUnit.start();
+      s.stop();
+      done();
+    }
+  });
+
+  window.scrollTo(0, 120);
+});
+
+
+QUnit.asyncTest('test scrollElement scrollTop tracker', function(assert) {
+  expect(1);
+  var scrollable = document.getElementById('scrollable');
+  var s = new Steady({
+    conditions: {
+      'min-scrollTop': 100,
+    },
+    scrollElement: scrollable,
+    handler: function(values, done) {
+      console.log(values);
+      assert.equal(values.scrollTop >= 101, true, 'scrollTop is okay' );
+      QUnit.start();
+      s.stop();
+      done();
+    }
+  });
+
+  scrollable.scrollTop += 120;
+});
+
+
+QUnit.asyncTest('test scrollElement scrollLeft tracker', function(assert) {
+  expect(1);
+  var scrollable = document.getElementById('scrollable');
+  var s = new Steady({
+    conditions: {
+      'min-scrollLeft': 100,
+    },
+    scrollElement: scrollable,
+    handler: function(values, done) {
+      console.log(values);
+      assert.equal(values.scrollLeft >= 101, true, 'scrollLeft is okay' );
+      QUnit.start();
+      s.stop();
+      done();
+    }
+  });
+
+  scrollable.scrollLeft += 120;
+});
+
+
+
+
+QUnit.testDone(function() {
+  window.scrollTo(0, 0);
+});
