2271dcdd91e54d7d2a65c0159aeb362e4c3cfdf6
David Clarke <davidcl64@gmail.com>
2014-06-17 10:47:51 -0700

    Basic gulp support for linking and qunit

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var gulp  = require('gulp');
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+gulp.task('scripts', function () {
+  return gulp.src('Steady.js')
+    .pipe($.jshint())
+    .pipe($.jshint.reporter(require('jshint-stylish')))
+    .pipe($.size());
+});
+
+gulp.task('connect', function () {
+  var connect = require('connect');
+  var app = connect()
+    .use(require('connect-livereload')({ port: 35729 }))
+    .use(connect.static(__dirname))
+    .use(connect.static('tests'));
+
+  require('http').createServer(app)
+    .listen(9000)
+    .on('listening', function () {
+      console.log('Started connect web server on http://localhost:9000');
+    });
+});
+
+gulp.task('serve', ['connect'], function () {
+  require('opn')('http://localhost:9000/tests/test.html');
+});
+
+gulp.task('watch', ['connect', 'serve'], function () {
+  var server = $.livereload();
+
+  // watch for changes
+
+  gulp.watch([
+    'Steady.js',
+    'tests/*.html',
+    'tests/*.js'
+  ]).on('change', function (file) {
+    server.changed(file.path);
+  });
+
+  gulp.watch('Steady.js', ['scripts']);
+});
+
+gulp.task('default', ['watch']);
